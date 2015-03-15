54774fcce25a83b0da149acb42f4c8e0aaa672c6
Oscar Lee Spencer III <spencero@ccs.neu.edu>
2015-03-15 00:50:11 -0400

    Making system calls happen

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,8 +3,24 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "devices/shutdown.h"
+#include "userprog/process.h"
+#include "lib/string.h"
 
 static void syscall_handler (struct intr_frame *);
+void halt (void);
+void exit (int);
+pid_t exec (const char *);
+int wait (pid_t);
+bool create (const char *, unsigned);
+bool remove (const char *);
+int open(const char *);
+int filesize (int);
+int read (int, void *, unsigned);
+int write (int, const void *, unsigned);
+void seek (int, unsigned);
+unsigned tell (int);
+void close (int);
 
 void
 syscall_init (void) 
@@ -17,4 +33,83 @@ syscall_handler (struct intr_frame *f UNUSED)
 {
   printf ("system call!\n");
   thread_exit ();
+}
+
+void
+halt (void)
+{
+  shutdown_power_off();
+}
+
+void
+exit (int status)
+{
+  process_exit ();
+}
+
+pid_t
+exec (const char *cmd_line)
+{
+  char *save_ptr;
+  return process_execute (strtok (cmd_line, " ", save_ptr));
+}
+
+int
+wait (pid_t pid)
+{
+  return process_wait (pid);
+}
+
+bool
+create (const char *file, unsigned size)
+{
+
+}
+
+bool
+remove (const char *file)
+{
+
+}
+
+int
+open (const char *file)
+{
+
+}
+
+int
+filesize (int fd)
+{
+
+}
+
+int
+read (int fd, void *buffer, unsigned size)
+{
+
+}
+
+int
+write (int fd, const void *buffer, unsigned size)
+{
+
+}
+
+void
+seek (int fd, unsigned position)
+{
+
+}
+
+unsigned 
+tell (int fd)
+{
+
+}
+
+void
+close (int fd)
+{
+
 }
