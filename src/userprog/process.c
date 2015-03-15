f123842c4a68b379d94609d1906d3f0598078507
nwotton <wottonn@gmail.com>
2015-03-15 19:05:58 -0400

    implemented argument passing

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -26,23 +26,41 @@ static bool load (const char *cmdline, void (**eip) (void), void **esp);
    before process_execute() returns.  Returns the new process's
    thread id, or TID_ERROR if the thread cannot be created. */
 tid_t
-process_execute (const char *file_name) 
+process_execute (const char *input)//used to be file_name 
 {
-  char *fn_copy;
+  char *fn_copy;  // function name copy
+  char *fi_copy = "";  // function inputs copy
   tid_t tid;
 
-  /* Make a copy of FILE_NAME.
-     Otherwise there's a race between the caller and load(). */
+  char *token, *save_ptr;
+  token = strtok_r (input, " ", &save_ptr);
   fn_copy = palloc_get_page (0);
   if (fn_copy == NULL)
     return TID_ERROR;
-  strlcpy (fn_copy, file_name, PGSIZE);
+  strlcpy (fn_copy, token, PGSIZE);
+  for(token = strtok_r (NULL, " ", &save_ptr); token != NULL;
+      token = strtok_r (NULL, " ", &save_ptr)){
+    strlcat(fi_copy, " ", 1);
+    strlcat(fi_copy, token, sizeof(token));
+  }
+  tid = thread_create (fn_copy, PRI_DEFAULT, start_process, fi_copy);
+  if (tid == TID_ERROR)
+    palloc_free_page (fn_copy);
+  return tid;
+
+
+  /* Make a copy of FILE_NAME.
+     Otherwise there's a race between the caller and load(). */
+//  fn_copy = palloc_get_page (0);
+//  if (fn_copy == NULL)
+//    return TID_ERROR;
+//  strlcpy (fn_copy, file_name, PGSIZE);
 
   /* Create a new thread to execute FILE_NAME. */
-  tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
-  if (tid == TID_ERROR)
-    palloc_free_page (fn_copy); 
-  return tid;
+//  tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
+//  if (tid == TID_ERROR)
+//    palloc_free_page (fn_copy); 
+//  return tid;
 }
 
 /* A thread function that loads a user process and starts it
