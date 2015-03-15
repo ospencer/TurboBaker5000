db2ec6ffe58a99d9cabd0bd436bf3942e371b75f
Oscar Lee Spencer III <spencero@ccs.neu.edu>
2015-03-15 01:42:35 -0400

    System handler be handlin'

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -6,6 +6,7 @@
 #include "devices/shutdown.h"
 #include "userprog/process.h"
 #include "lib/string.h"
+#include "lib/syscall-nr.h"
 
 static void syscall_handler (struct intr_frame *);
 void halt (void);
@@ -32,7 +33,85 @@ static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
   printf ("system call!\n");
-  thread_exit ();
+  void *esp = f->esp;
+  switch (&esp)
+  {
+  case SYS_HALT:
+    halt ();
+    break;
+  case SYS_EXIT:
+    esp += 4;
+    int status = &esp;
+    exit (status);
+    break;
+  case SYS_EXEC:
+    esp += 4;
+    const char *cmd = esp;
+    exec (cmd);
+    break;
+  case SYS_WAIT:
+    esp += 4;
+    pid_t pid = &esp;
+    wait (pid);
+    break;
+  case SYS_CREATE:
+    esp += 4;
+    const char *file = esp;
+    esp += 4;
+    unsigned size = &esp;
+    create (file, size);
+    break;
+  case SYS_REMOVE:
+    esp += 4;
+    const char *file = esp;
+    remove (file);
+    break;
+  case SYS_OPEN:
+    esp += 4;
+    const char *file = esp;
+    open (file);
+    break;
+  case SYS_FILESIZE:
+    esp += 4;
+    int fd = &esp;
+    filesize (fd);
+    break;
+  case SYS_READ:
+    esp += 4;
+    int fd = &esp;
+    esp += 4;
+    void *buffer = &esp;
+    esp += 4;
+    unsigned size = &esp;
+    read (fd, buffer, size);
+    break;
+  case SYS_WRITE: 
+    esp += 4;
+    int fd = &esp;
+    esp += 4;
+    void *buffer = &esp;
+    esp += 4;
+    unsigned size = &esp;
+    write (fd, buffer, size);
+    break;
+  case SYS_SEEK:
+    esp += 4;
+    int fd = &esp;
+    esp += 4;
+    unsigned position = &esp;
+    seek (fd, position);
+    break;
+  case SYS_TELL:
+    esp += 4;
+    int fd = &esp;
+    tell (fd);
+    break;
+  case SYS_CLOSE:
+    esp += 4;
+    int fd = &esp;
+    close (fd);
+    break;
+  }
 }
 
 void
@@ -51,7 +130,7 @@ pid_t
 exec (const char *cmd_line)
 {
   char *save_ptr;
-  return process_execute (strtok (cmd_line, " ", save_ptr));
+  return process_execute (strtok_r (cmd_line, " ", save_ptr));
 }
 
 int
