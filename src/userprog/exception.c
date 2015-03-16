8d6bf3d7b3586f541844f51e415b884e609f4037
Oscar Lee Spencer III <spencero@ccs.neu.edu>
2015-03-16 15:27:28 -0400

    It makes now

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -151,13 +151,13 @@ page_fault (struct intr_frame *f)
 
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
-     which fault_addr refers.
+     which fault_addr refers.*/
   printf ("Page fault at %p: %s error %s page in %s context.\n",
           fault_addr,
           not_present ? "not present" : "rights violation",
           write ? "writing" : "reading",
           user ? "user" : "kernel");
-  kill (f);*/
-  page_lookup (active_pd (void), fault_addr, true);
+  kill (f);
+  //page_lookup (active_pd (void), fault_addr, true);
 }
 
