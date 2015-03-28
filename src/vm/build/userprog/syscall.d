734d180503f6a494f3d0b1c026615a6fde3ec7e8
Oscar Lee Spencer III <spencero@ccs.neu.edu>
2015-03-27 21:56:34 -0400

    Added .gitignore

---
+++ src/vm/build/userprog/syscall.d
@@ -0,0 +1,9 @@
+userprog/syscall.o: ../../userprog/syscall.c ../../userprog/syscall.h \
+ ../../lib/stdio.h ../../lib/debug.h ../../lib/stdarg.h \
+ ../../lib/stdbool.h ../../lib/stddef.h ../../lib/stdint.h \
+ ../../lib/kernel/stdio.h ../../lib/syscall-nr.h \
+ ../../threads/interrupt.h ../../threads/thread.h ../../lib/kernel/list.h \
+ ../../filesys/file.h ../../filesys/off_t.h ../../devices/shutdown.h \
+ ../../userprog/process.h ../../lib/string.h ../../lib/syscall-nr.h \
+ ../../threads/vaddr.h ../../threads/loader.h ../../userprog/pagedir.h \
+ ../../threads/palloc.h
