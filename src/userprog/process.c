e7b87a3edd6c4bbf50a5d534957e738b38a4650d
nwotton <wottonn@gmail.com>
2015-04-01 00:32:33 -0400

    quick change

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -424,7 +424,7 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
       size_t page_zero_bytes = PGSIZE - page_read_bytes;
 
       /* Get a page of memory. */
-      uint8_t *kpage = add_frame_entry(PAL_USER);// palloc_get_page (PAL_USER);
+      uint8_t *kpage = add_frame_entry(false);// palloc_get_page (PAL_USER);
       if (kpage == NULL)
         return false;
 
